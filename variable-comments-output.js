0343dd20a13ece496fdbff894a9f1a3978acdebc
Mahadi Hasan <73481549+nfmahadi@users.noreply.github.com>
2025-01-29 23:35:13 +0600

    Day 1 - JavaScript Comments, Variable and output.

---
+++ variable-comments-output.js
@@ -0,0 +1,26 @@
+// comments
+//----------
+
+// This is a Single Line Comment
+
+/*
+* This is an example of multi-line comments.
+* Comments are used to make code more readable
+* or explain a block of code.
+* Comments are also used for preventing the execution of code.
+*/
+
+// Printing output to the console
+//--------------------------
+
+console.log("Hello World!");
+
+
+// Variable
+//----------
+
+let user = "Albert";
+let age = 30;
+const birthPlace = "Rome";
+
+console.log(user + " is " + age + " years old. He was born in " + birthPlace);
